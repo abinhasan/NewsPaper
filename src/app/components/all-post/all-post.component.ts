2f83d6a84a3024a465aa92ee31283ace37000b0a
abinhasan <porag214@gmail.com>
2017-06-20 23:17:20 +0600

    add allpost component

---
+++ src/app/components/all-post/all-post.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+//import * as firebase from 'firebase/app';
+import { FirebaseService } from '../../services/firebase.service';
+
+@Component({
+  selector: 'app-all-post',
+  templateUrl: './all-post.component.html',
+  styleUrls: ['./all-post.component.css']
+})
+export class AllPostComponent implements OnInit {
+  allPostArray: any;
+
+  constructor(
+    private firebaseService: FirebaseService
+  ) { }
+
+  ngOnInit() {
+    this.firebaseService.getAllPosts().subscribe(allPosts => {
+
+      let data = allPosts.sort((n1, n2) => {
+        return n2.startedAt - n1.startedAt;
+      });
+
+      let allPostArray = [];
+
+      for (let i in data) {
+
+        let date = new Date(data[i].startedAt);
+
+        allPostArray.push({
+          key: data[i].$key,
+          title: data[i].title,
+          description: data[i].description,
+          type: data[i].type,
+          imageUrl: data[i].imageUrl,
+          view: data[i].view,
+          like: data[i].like,
+          startedAt: date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+        });
+      }
+
+      this.allPostArray = allPostArray;
+      //console.log(allPostArray);
+    });
+  }
+
+}
