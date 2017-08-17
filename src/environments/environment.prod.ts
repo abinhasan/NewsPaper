025b62607c3b2b834d358f7da2f78c070ce41fe7
porag <porag214@gmail.com>
2017-08-17 11:16:56 +0600

    change /environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAvJbkbgnni-vI47reZ87gXLv6NknAVzf0",
+    authDomain: "news-46134.firebaseapp.com",
+    databaseURL: "https://news-46134.firebaseio.com",
+    projectId: "news-46134",
+    storageBucket: "news-46134.appspot.com",
+    messagingSenderId: "818612820930"
+  }
 };
